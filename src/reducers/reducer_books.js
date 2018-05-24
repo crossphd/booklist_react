9189b43b84c6be59cfd0f68d1c3cdec0e08e4b78
crossphd <cross25@uis.edu>
2018-05-23 17:45:19 -0700

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function() {
+	return [
+		{ title: 'Javascript: The Good Parts', pages: 101 },
+		{ title: 'Harry Potter', pages: 22 },
+		{ title: 'The Dark Tower', pages: 333 },
+		{ title: 'Eloquent Ruby', pages: 1 }
+	]
+}
